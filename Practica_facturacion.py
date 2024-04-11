a50ac2f5111e22caa3232642bd1c5796035661d6
DianaGMD <a01368899@tec.mx>
2024-04-10 18:20:38 -0600

    Practica facturacion

--- Practica_facturacion.py
+++ Practica_facturacion.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_excel("datos_facturacion.xlsx)
+df = pd.read_excel("datos_facturacion.xlsx")
 print(df.head())
 
 filtro1=df[(df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"]<=2000)]
 print(filtro1)
 filtro1.to_csv("Practica_facturacion1.csv")
+
 
