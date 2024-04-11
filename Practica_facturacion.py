0765500e7b86c8618b107167990bbb3d7f9851b2
DianaGMD <a01368899@tec.mx>
2024-04-10 19:55:03 -0600

    Lo que faltaba

--- Practica_facturacion.py
+++ Practica_facturacion.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 
 df = pd.read_excel("datos_facturacion.xlsx")
@@ -14,9 +15,16 @@ filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
 filtro2.to_csv("Practica_facturacion2.csv")
 
 #FECHA_ENT   (Fechas de “28/02/2022”)
-#filtro3=df[df["FECHA_ENT"].dt.strftime('%d-%m-%Y') == '03-09-2019']
+# year
+#filtro3=df[ df["FECHA_ENT"].dt.strftime('%Y') == '2019']
+# year-month
+#filtro3=df[ df["FECHA_ENT"].dt.strftime('%Y-%m') == '2019-10']
+# year-month-day
+#df['FECHA_ENT'] = pd.to_datetime(df['FECHA_ENT'])
+df['FECHAELAB'] = pd.to_datetime(df['FECHAELAB'])
+filtro3=df[ df["FECHAELAB"].dt.strftime('%Y-%m-%d') == '2019-10-02']
 #print(filtro3)
-#filtro3.to_csv("Practica_facturacion3.csv")
+filtro3.to_csv("Practica_facturacion3.csv")
 
 #CAN_TOT       (Cantidades menores a 5951.7)   o  STATUS (“E”)
 filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
@@ -30,6 +38,14 @@ filtro5.to_csv("Practica_facturacion5.csv")
 
 #Solo las filas de 7001-7099
 filtro6 = df.iloc[7001:7099, :  ]
-print(filtro6)
+#print(filtro6)
 filtro6.to_csv("Practica_facturacion6.csv")
+
+#Index= CVE_VEND (valores: 1, 2)  (columna: FECHAELAB)
+df1= pd.read_excel('datos_facturacion.xlsx', index_col=3)
+df1.head()
+filtro7=df1.loc[[1.0,2.0], ["FECHAELAB"]]
+print(filtro7)
+filtro7.to_csv("Practica_facturacion7.csv")
+
 
