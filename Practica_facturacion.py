7aaf6e09d9d5cb3d6787836af478eb574351d02b
DianaGMD <a01368899@tec.mx>
2024-04-10 18:26:36 -0600

    filtro 2 y 3

--- Practica_facturacion.py
+++ Practica_facturacion.py
@@ -7,4 +7,12 @@ filtro1=df[(df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"]<=2000)]
 print(filtro1)
 filtro1.to_csv("Practica_facturacion1.csv")
 
+filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
+print(filtro2)
+filtro2.to_csv("Practica_facturacion2.csv")
+
+filtro3=df[df["FECHA_ENT"] == '2022-28-2']
+print(filtro3)
+filtro3.to_csv("Practica_facturacion3.csv")
+
 
