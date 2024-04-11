956140562f3478e537c58f08f8f012368d6518f4
DianaGMD <a01368899@tec.mx>
2024-04-10 18:55:33 -0600

    Complemento

--- Practica_facturacion.py
+++ Practica_facturacion.py
@@ -14,16 +14,22 @@ filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
 filtro2.to_csv("Practica_facturacion2.csv")
 
 #FECHA_ENT   (Fechas de “28/02/2022”)
-filtro3=df[df["FECHA_ENT"] == '2022-28-2']
+#filtro3=df[df["FECHA_ENT"].dt.strftime('%d-%m-%Y') == '03-09-2019']
 #print(filtro3)
-filtro3.to_csv("Practica_facturacion3.csv")
+#filtro3.to_csv("Practica_facturacion3.csv")
 
 #CAN_TOT       (Cantidades menores a 5951.7)   o  STATUS (“E”)
 filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
-print(filtro4)
+#print(filtro4)
 filtro4.to_csv("Practica_facturacion4.csv")
 
+#Solo las columnas: CVE_DOC, FECHA_ENT, FECHA_VEN y CAN_TOT
+filtro5 = df.iloc[ : , [0, 6, 7,9]]
+#print(filtro5)
+filtro5.to_csv("Practica_facturacion5.csv")
 
-
-
+#Solo las filas de 7001-7099
+filtro6 = df.iloc[7001:7099, :  ]
+print(filtro6)
+filtro6.to_csv("Practica_facturacion6.csv")
 
